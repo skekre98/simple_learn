251fde5dc3f152a29c3fae8f29f10ba63c8b46d9
Sharvil Kekre <sharvilkekre@gmail.com>
2020-12-12 19:15:54 -0800

    fixed tests

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -82,5 +82,5 @@ function build_and_release {
 }
 
 function run_tests {
-    python -W -m unittest tests/classifiers/simple_classifier_tests.py -v
+    python -W ignore -m unittest tests/classifiers/simple_classifier_tests.py -v
 }
