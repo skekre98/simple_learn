3ce5643ae398276d96cd92d7df93caebd80a6d7a
skekre98 <sharvil.kekre@nutanix.com>
2020-12-23 12:31:55 -0800

    New release

--- setup.py
+++ setup.py
@@ -40,7 +40,12 @@ setuptools.setup(
     packages=["simple_learn"],
     install_requires=requirements,
     classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
