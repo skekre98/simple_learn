416b6cb06fe0baff0406f894e482327ef5c835b4
eran.saranga@gmail.com <eran.saranga@gmail.com>
2021-01-13 13:06:10 +0200

    wrong test name in custom.sh script

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -85,5 +85,6 @@ function build_and_release {
 function run_tests {
     python -W ignore -m unittest tests/classifiers/simple_classifier_tests.py -v
     python -W ignore -m unittest tests/classifiers/simple_classifier_list_tests.py -v
-    python -W ignore -m unittest tests/regressors/simple_classifier_regressor_tests.py -v
+    python -W ignore -m unittest tests/regressors/simple_regressor_tests.py -v
+
 }
